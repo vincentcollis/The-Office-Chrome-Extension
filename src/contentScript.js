b1c9bcda7b66193c75ff9c0be5ebc9b96d96cc47
Joyce Oh <joyce.oh@live.com>
2024-07-11 18:04:14 -0400

    working main without url change

---
+++ src/contentScript.js
@@ -0,0 +1,13 @@
+console.log("hereweare")
+const button = document.querySelector('#answer');
+
+chrome.runtime.onMessage.addListener(gotMessage);
+function gotMessage(message,sender,sendresponse)
+{
+	console.log(message.txt);
+	let paragraphs = document.getElementsByTagName("p");
+	for(elt of paragraphs)
+	{
+		elt.style['background-color'] = '#00CED1';
+	}
+}
